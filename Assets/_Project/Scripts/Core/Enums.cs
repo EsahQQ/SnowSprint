ebb8a97f462f3b1d84bbce531ad57043ef0b19fb
Kovalev Ivan <kovalevvanc@gmail.com>
2026-01-30 12:39:32 +0300

    Add: so for shopitem

---
+++ Assets/_Project/Scripts/Core/Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace _Project.Scripts.Utils
+{
+    public class Enums
+    {
+        public enum UpgradeType
+        {
+            Speed,
+            Acceleration,
+            Skin
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Data/ShopItemConfig.cs
@@ -0,0 +1,17 @@
+﻿using _Project.Scripts.Utils;
+using UnityEngine;
+
+namespace _Project.Scripts.Data
+{
+    [CreateAssetMenu(fileName = "ShopItem", menuName = "SO/New Item")]
+    public class ShopItemConfig : ScriptableObject
+    {
+        public string Id;
+        public string DisplayName;
+        public int Price;
+        public Sprite Icon;
+
+        public Enums.UpgradeType Type;
+        public float Value;
+    }
+}
